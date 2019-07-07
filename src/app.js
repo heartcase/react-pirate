c232e9ec74bad5717d94911e42e36d03e2d56f95
heartcase <lby920101@gmail.com>
2019-07-07 14:25:55 +0800

    #6 Update Router
    1.add sitemap configuration for routing
    2.update the demo to show this functionality
    
    something still raw and messy here
    might be improved in future updates

--- src/app.js
+++ src/app.js
@@ -1,21 +1,16 @@
-
 import React from 'react'
-import { Switch, Route, Link } from 'react-router-dom'
-import Page from './containers/page'
+import { Link } from 'react-router-dom'
+import router from './router'
 
 const renderProps = (Components, props={}) => (() => <Components { ...props } />)
 
-const App  = () => (
+const App  = props => (
   <div>
+    { Object.keys(props) + '' }
     <Link to="/">Home</Link><br/>
-    <Link to="/page-1">Page 1</Link><br/>
-    <Link to="/page-2">Page 2</Link><br/>
-    <Switch>
-      <Route exact path="/" render={renderProps(Page, {pageName: 'Home'})} />
-      <Route exact path="/page-1" render={renderProps(Page, {pageName: 'Page 1'})} />
-      <Route exact path="/page-2" render={renderProps(Page, {pageName: 'Page 2'})} />
-      <Route render={renderProps(Page, {pageName: 'Not Found Page'})} />
-    </Switch>
+    <Link to="/pages/">Pages/</Link><Link to="/pages/page-1">Page 1</Link><br/>
+    <Link to="/pages/">Pages/</Link><Link to="/pages/page-2">Page 2</Link><br/>
+    { router }
   </div>
 )
 
---
+++ src/router.js
@@ -0,0 +1,56 @@
+import React from 'react'
+import Page from 'containers/page'
+import { Switch, Route } from 'react-router-dom'
+
+const renderProps = (Components, props={}) => (() => <Components { ...props } />)
+
+// define a tree like mapping for the website
+// the properties is equivalent to the <Route> props
+export const sitemap = [
+  {
+    path: '/',
+    exact: true,
+    render: renderProps(Page, {pageName: 'Home'})
+  },
+  {
+    path: '/pages',
+    exact: true,
+    render: renderProps(Page, {pageName: 'Pages'}),
+    routes: [
+      {
+        path: '/pages/page-1',
+        exact: true,
+        render: renderProps(Page, {pageName: 'Page-1'}),
+      },
+      {
+        path: '/pages/page-2',
+        exact: true,
+        render: renderProps(Page, {pageName: 'Page-2'}),
+      },      
+    ]
+  },
+  // catch unmatched path
+  {
+    render: renderProps(Page, {pageName: 'Page Not Found'})
+  }
+]
+
+// transform to a flat array
+const routes = []
+const addRoutes = route => {
+  routes.push(route)
+  if (route.routes) {
+    route.routes.forEach(addRoutes);
+  }
+}
+sitemap.forEach(addRoutes)
+
+export default (
+  <Switch>
+    {
+      routes.map(
+        route => <Route {...route} key={route.path || 'not-found'}/>
+      )
+    }
+  </Switch>
+)
